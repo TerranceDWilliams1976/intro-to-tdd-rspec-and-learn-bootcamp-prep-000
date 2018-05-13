cd90018b37baebe8871d9d375c7b9fe38c5e0ba9
Terrance Williams <terrance.d.williams1976@gmail.com>
2018-05-13 22:53:54 +0000

    automatically backed up by learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -10,8 +10,7 @@ describe "current_age_for_birth_year method" do
     current_year = Time.now.year
     birth_year = 1984
     answer = current_year - birth_year
-    age_of_person = current_age_for_birth_year(1984)
-
-    expect(age_of_person).to eq(19)
+    age_of_person = current_age_for_birth_year(birth_year)
+    expect(age_of_person).to eq(answer)
   end
 end
